9219d9fd1c3826de649a6c074a6812b624b7ec36
reireirei1221 <reireirei1221@icloud.com>
2023-01-01 17:58:20 +0900

    Watch connectivity

--- Distraction Journal Watch App/ExtensionDelegate.swift
+++ Distraction Journal Watch App/ExtensionDelegate.swift
@@ -5,19 +5,29 @@
 //  Created by 澤野令 on 2022/12/29.
 //
 
-import WatchKit
-import Combine
+import Foundation
 import WatchConnectivity
 
 class ExtensionDelegate: NSObject {
     
     private let session: WCSession
-    private let delegate: WCSessionDelegate
+    
+    init(session: WCSession  = .default) {
+        self.session = session
+        super.init()
+        self.session.delegate = self
+        session.activate()
+    }
 
-    
-    
-    
-    
-    
-    
+}
+
+extension ExtensionDelegate: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        if let error = error {
+            // Error handring if need
+            print(error.localizedDescription)
+        } else {
+            print("The session has completed activation.")
+        }
+    }
 }
